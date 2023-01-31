90cded778594e87b6131a7d3e9768a32004b1d2a
mkhfahad00 <fahad.khawaja@mergestack.com>
2023-01-31 11:47:49 +0500

    added redux-connect and reselector

--- src/containers/inputModalContainer.tsx
+++ src/containers/inputModalContainer.tsx
@@ -1,44 +1,73 @@
-import React, { useCallback } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
+import { connect } from "react-redux";
 import { addStudent, updateStudent } from "state/ducks/students/actions";
 import { IStudentRaw } from "state/ducks/students/types";
 import StudentInputModal from "components/inputModal";
+import { ActionType } from "typesafe-actions";
 
 interface IModalContainerProps {
   visible: boolean;
   setVisible: (x: boolean) => void;
   studentData: IStudentRaw;
   setStudentData: (std: IStudentRaw | null) => void;
+  addStudent: (payload: IStudentRaw) => ActionType<typeof addStudent>;
+  updateStudent: (payload: IStudentRaw) => ActionType<typeof updateStudent>;
 }
-
 const InputModalContainer = ({
   visible,
   setVisible,
   studentData,
   setStudentData,
+  addStudent,
+  updateStudent,
 }: IModalContainerProps) => {
-  const dispatch = useDispatch();
-
-  const dispatchToProps = {
-    addStudent: useCallback(
-      (payload: IStudentRaw) => dispatch(addStudent(payload)),
-      [dispatch]
-    ),
-    updateStudent: useCallback(
-      (payload: IStudentRaw) => dispatch(updateStudent(payload)),
-      [dispatch]
-    ),
-  };
-
   return (
     <StudentInputModal
       visible={visible}
       setVisible={setVisible}
       studentData={studentData as IStudentRaw}
       setStudentData={setStudentData}
-      {...dispatchToProps}
+      addStudent={addStudent}
+      updateStudent={updateStudent}
     />
   );
 };
 
-export default InputModalContainer;
+const mapDispatchToProps = {
+  addStudent,
+  updateStudent,
+};
+
+export default connect(null, mapDispatchToProps)(InputModalContainer);
+
+// const InputModalContainer = ({
+//   visible,
+//   setVisible,
+//   studentData,
+//   setStudentData,
+// }: IModalContainerProps) => {
+//   const dispatch = useDispatch();
+
+//   const dispatchToProps = {
+//     addStudent: useCallback(
+//       (payload: IStudentRaw) => dispatch(addStudent(payload)),
+//       [dispatch]
+//     ),
+//     updateStudent: useCallback(
+//       (payload: IStudentRaw) => dispatch(updateStudent(payload)),
+//       [dispatch]
+//     ),
+//   };
+
+//   return (
+//     <StudentInputModal
+//       visible={visible}
+//       setVisible={setVisible}
+//       studentData={studentData as IStudentRaw}
+//       setStudentData={setStudentData}
+//       {...dispatchToProps}
+//     />
+//   );
+// };
+
+// export default InputModalContainer;
--- src/containers/mainViewContainer.tsx
+++ src/containers/mainViewContainer.tsx
@@ -1,26 +1,64 @@
-import React, { useCallback } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { connect } from "react-redux";
 import MainView from "components/mainView";
 import { IApplicationState } from "state/ducks/index";
 import { fetchStudents } from "state/ducks/students/actions";
 import { IStudentState } from "state/ducks/students/types";
+import { ActionType } from "typesafe-actions";
+import { getSummaryData } from "state/ducks/students/reselectors";
+import { ISummaryData } from "utils";
 
-const MainViewContainer = () => {
-  const dispatch = useDispatch();
+interface IContainerProps extends IStudentState {
+  fetchStudents: () => ActionType<typeof fetchStudents>;
+  dashboardData: ISummaryData;
+}
+const mapStateToProps = (state: IApplicationState) => ({
+  loading: state.students.loading,
+  errors: state.students.errors,
+  data: state.students.data,
+  dashboardData: getSummaryData(state),
+});
 
-  const stateToProps: IStudentState = useSelector(
-    ({ students }: IApplicationState) => ({
-      loading: students.loading,
-      errors: students.errors,
-      data: students.data,
-    })
-  );
-
-  const dispatchToProps = {
-    fetchStudents: useCallback(() => dispatch(fetchStudents()), [dispatch]),
-  };
-
-  return <MainView {...stateToProps} {...dispatchToProps} />;
+const mapDispatchToProps = {
+  fetchStudents,
 };
 
-export default MainViewContainer;
+const MainViewContainer = ({
+  data,
+  fetchStudents,
+  errors,
+  loading,
+  dashboardData,
+}: // dashboardData,
+
+IContainerProps) => (
+  <MainView
+    data={data}
+    fetchStudents={fetchStudents}
+    loading={loading}
+    errors={errors}
+    dashboardData={dashboardData}
+  />
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainViewContainer);
+
+// const MainViewContainer = () => {
+//   const dispatch = useDispatch();
+
+//   const stateToProps: IStudentState = useSelector(
+//     ({ students }: IApplicationState) => ({
+//       loading: students.loading,
+//       errors: students.errors,
+//       data: students.data,
+//     })
+//   );
+
+//   const dispatchToProps = {
+//     fetchStudents: useCallback(() => dispatch(fetchStudents()), [dispatch]),
+//   };
+
+//   return <MainView {...stateToProps} {...dispatchToProps} />;
+// };
+
+// export default MainViewContainer;
