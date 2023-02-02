0376f49c8e9d5f1178c5157b1af41abb669f4ad3
mkhfahad00 <fahad.khawaja@mergestack.com>
2023-02-02 15:35:37 +0500

    updated connectors implementation

--- src/containers/inputModalContainer.tsx
+++ src/containers/inputModalContainer.tsx
@@ -1,41 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
 import { addStudent, updateStudent } from "state/ducks/students/actions";
-import { IStudentRaw } from "state/ducks/students/types";
 import StudentInputModal from "components/inputModal";
-import { ActionType } from "typesafe-actions";
-
-interface IModalContainerProps {
-  visible: boolean;
-  setVisible: (x: boolean) => void;
-  studentData: IStudentRaw;
-  setStudentData: (std: IStudentRaw | null) => void;
-  addStudent: (payload: IStudentRaw) => ActionType<typeof addStudent>;
-  updateStudent: (payload: IStudentRaw) => ActionType<typeof updateStudent>;
-}
-const InputModalContainer = ({
-  visible,
-  setVisible,
-  studentData,
-  setStudentData,
-  addStudent,
-  updateStudent,
-}: IModalContainerProps) => {
-  return (
-    <StudentInputModal
-      visible={visible}
-      setVisible={setVisible}
-      studentData={studentData as IStudentRaw}
-      setStudentData={setStudentData}
-      addStudent={addStudent}
-      updateStudent={updateStudent}
-    />
-  );
-};
 
 const mapDispatchToProps = {
   addStudent,
   updateStudent,
 };
 
-export default connect(null, mapDispatchToProps)(InputModalContainer);
+export default connect(null, mapDispatchToProps)(StudentInputModal);
